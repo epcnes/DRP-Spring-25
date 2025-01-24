3de7795e98b0c46cefed7aee586d941856b92ebf
Stephen <stephenhu2000@gmail.com>
2025-01-24 15:39:24 -0500

    added things to gitignore, changed to bar

--- Code/experiment.py
+++ Code/experiment.py
@@ -10,7 +10,8 @@ terms = []
 freqs = []
 means = []
 for i in range(size):
-    r = np.float64(np.sqrt(1/np.random.rand()))
+    #r = np.float64(np.sqrt(1/np.random.rand()))
+    r = np.float64(np.random.rand())
     a, b = getData(r)
     terms.append(a.keys())
     freqs.append(a.values())
@@ -28,9 +29,9 @@ plt99.set_title(means[int(size/4)], loc='center', pad=0.5)
 plt499.set_title(means[int(size/2)], loc='center', pad=0.5)
 plt999.set_title(means[-1], loc='center', pad=0.5)
 
-plt0.scatter(terms[0], freqs[0])
-plt99.scatter(terms[int(size/4)], freqs[int(size/4)])
-plt499.scatter(terms[int(size/2)], freqs[int(size/2)])
-plt999.scatter(terms[-1], freqs[-1])
+plt0.bar(terms[0], freqs[0])
+plt99.bar(terms[int(size/4)], freqs[int(size/4)])
+plt499.bar(terms[int(size/2)], freqs[int(size/2)])
+plt999.bar(terms[-1], freqs[-1])
 
 plt.show()
